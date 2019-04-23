b5f443ad86c2015443dbdbd8d96475142385b1d0
Bruno Ramalho <bramalho@divante.pl>
2019-04-23 11:15:18 +0200

    init ClassificationTreeBuilderTest Mocks

--- tests/ClassificationTreeBundle/Service/ClassificationTreeBuilderTest.php
+++ tests/ClassificationTreeBundle/Service/ClassificationTreeBuilderTest.php
@@ -2,10 +2,25 @@
 
 namespace Divante\ClassificationTreeBundle\Service;
 
+use AdvancedObjectSearchBundle\Service;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class ClassificationTreeBuilderTest extends TestCase
 {
+    /** @var ClassificationTreeBuilder $classificationTreeBuilder */
+    private $classificationTreeBuilder;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        /** @var Service|MockObject $searchService */
+        $searchService = $this->createMock(Service::class);
+
+        $this->classificationTreeBuilder = new ClassificationTreeBuilder($searchService);
+    }
+
     public function testSomething()
     {
         $this->assertTrue(true);
